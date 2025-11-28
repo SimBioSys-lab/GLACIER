bab91f025e1c7a5abdba313589d5c23097cbfa87
SimBioSys-lab <s.chakraborty@northeastern.edu>
2025-11-27 19:56:46 -0600

    Update bot.py

--- mcp-logs/bot.py
+++ mcp-logs/bot.py
@@ -215,4 +215,5 @@ def slack_events():
     return handler.handle(request)
 
 if __name__ == "__main__":
-    flask_app.run(host='0.0.0.0', port=3000)
+    port = int(os.environ.get("PORT", 3000))
+    flask_app.run(host='0.0.0.0', port=port)
