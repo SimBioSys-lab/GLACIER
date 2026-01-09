da92c4fa153a9d6d976699f6d5942da453f57ce6
SimBioSys-lab <s.chakraborty@northeastern.edu>
2026-01-09 13:27:12 -0600

    Add files via upload

--- backend/app/models/schemas.py
+++ backend/app/models/schemas.py
@@ -4,8 +4,8 @@ from typing import List, Optional
 class UploadResponse(BaseModel):
     status: str
     message: str
-    email: str
-    name: str
+    email: Optional[str] = None
+    name: Optional[str] = None
     files: List[str]
     job_ids: List[str]
     user_id: str
